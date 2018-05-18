7ce7b5686caa641da7da1c7e52912fe513830d65
T-101 <T-101@PC213.mshome.net>
2018-05-17 19:45:30 -0500

    Solucion converison parte 1

---
+++ Objetos-1803/src/abstraccion/Grados.java
@@ -0,0 +1,23 @@
+package abstraccion;
+
+public class Grados {
+    private float centigrados;
+    private float farenheit;
+
+    public float getCentigrados() {
+        return centigrados;
+    }
+
+    public void setCentigrados(float centigrados) {
+        this.centigrados = centigrados;
+    }
+
+    public float getFarenheit() {
+        return farenheit;
+    }
+
+    public void setFarenheit(float farenheit) {
+        this.farenheit = farenheit;
+    }
+    
+}
